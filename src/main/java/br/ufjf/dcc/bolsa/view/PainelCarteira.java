1a755355b1294f82426507259902146bee67f7a1
gleiph <gleiphgh@gmail.com>
2021-04-25 19:22:43 -0300

    Removendo comentários e imports desnecessários

--- src/main/java/br/ufjf/dcc/bolsa/view/PainelCarteira.java
+++ src/main/java/br/ufjf/dcc/bolsa/view/PainelCarteira.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufjf.dcc.bolsa.view;
 
 import br.ufjf.dcc.bolsa.Dados;
 import br.ufjf.dcc.bolsa.model.Ativo;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
