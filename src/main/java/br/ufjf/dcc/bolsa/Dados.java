8b04b4554c15a6bd3794aa80c97f60861f806f35
gleiph <gleiphgh@gmail.com>
2021-05-17 20:10:57 -0300

    Persistindo Carteiras em Arquivos

--- src/main/java/br/ufjf/dcc/bolsa/Dados.java
+++ src/main/java/br/ufjf/dcc/bolsa/Dados.java
@@ -1,20 +1,18 @@
 package br.ufjf.dcc.bolsa;
 
-import br.ufjf.dcc.bolsa.model.Ativo;
 import br.ufjf.dcc.bolsa.model.Carteira;
 import br.ufjf.dcc.bolsa.model.Negociacao;
 import br.ufjf.dcc.bolsa.utils.Arquivo;
 import br.ufjf.dcc.bolsa.utils.PropertyBasedInterfaceMarshal;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import java.lang.reflect.Type;
-import com.google.gson.reflect.TypeToken;
 
 /**
  *
@@ -22,20 +20,14 @@ import com.google.gson.reflect.TypeToken;
  */
 public class Dados {
 
-    private static final String ATIVOS_PATH = "Ativos.json";
-    private static final String NEGOCIACOES_PATH = "Negociacoes.json";
+    private static final String CARTEIRAS_PATH = "Carteiras.json";
 
-    
     private static Carteira carteiraAtiva;
     private static List<Carteira> carteiras;
-//    private static List<Ativo> ativos;
-//    private static List<Negociacao> negociacoes;
 
     static {
         carteiraAtiva = null;
         carteiras = new ArrayList<>();
-//        ativos = new ArrayList<>();
-//        negociacoes = new ArrayList<>();
     }
 
     public static Carteira getCarteiraAtiva() {
@@ -53,89 +45,48 @@ public class Dados {
     public static void setCarteiras(List<Carteira> carteiras) {
         Dados.carteiras = carteiras;
     }
-    
+
     public static Carteira getCarteira(String nome) {
         for (Carteira carteira : carteiras) {
             if (carteira.getNome().equals(nome)) {
                 return carteira;
             }
         }
-        
+
         return null;
     }
 
-
-//    public static List<Ativo> getAtivos() {
-//        return ativos;
-//    }
-//
-//    public static void setAtivos(List<Ativo> ativos) {
-//        Dados.ativos = ativos;
-//    }
-//
-//    public static List<Negociacao> getNegociacoes() {
-//        return negociacoes;
-//    }
-//
-//    public static void setNegociacoes(List<Negociacao> negociacoes) {
-//        Dados.negociacoes = negociacoes;
-//    }
-
-//    public static Ativo getAtivo(String tag) {
-//        for (Ativo ativo : ativos) {
-//            if (ativo.getTag().equals(tag)) {
-//                return ativo;
-//            }
-//        }
-//
-//        return null;
-//    }
-
     public static void salvar() {
-//        try {
-//            Gson gson = new Gson();
-//            String jsonAtivos = gson.toJson(ativos);
-//            Arquivo.gravarArquivo(ATIVOS_PATH, jsonAtivos);
-//
-//            gson = new GsonBuilder()
-//                    .registerTypeAdapter(Negociacao.class,
-//                            new PropertyBasedInterfaceMarshal())
-//                    .create();
-//            
-//            Type listNegociacoes = new TypeToken<ArrayList<Negociacao>>() {
-//            }.getType();
-//            
-//            String jsonNegociacoes = gson.toJson(negociacoes, listNegociacoes);
-//            Arquivo.gravarArquivo(NEGOCIACOES_PATH, jsonNegociacoes);
-//
-//        } catch (IOException ex) {
-//            Logger.getLogger(Dados.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(Negociacao.class,
+                            new PropertyBasedInterfaceMarshal())
+                    .create();
+
+            Arquivo.gravarArquivo(CARTEIRAS_PATH, gson.toJson(carteiras));
+
+        } catch (IOException ex) {
+            Logger.getLogger(Dados.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
     public static void recuperar() {
-//        try {
-//            String conteudoAtivo = Arquivo.lerArquivo(ATIVOS_PATH);
-//            Gson gson = null;
-//
-//            Type listAtivos = new TypeToken<ArrayList<Ativo>>() {
-//            }.getType();
-//            ativos = new Gson().fromJson(conteudoAtivo, listAtivos);
-//
-//            String conteudoNegociacoes = Arquivo.lerArquivo(NEGOCIACOES_PATH);
-//            Type listNegociacoes = new TypeToken<ArrayList<Negociacao>>() {
-//            }.getType();
-//
-//            gson = new GsonBuilder()
-//                    .registerTypeAdapter(Negociacao.class,
-//                            new PropertyBasedInterfaceMarshal())
-//                    .create();
-//
-//            negociacoes = gson.fromJson(conteudoNegociacoes, listNegociacoes);
-//
-//        } catch (IOException ex) {
-//            System.out.println("Arquivo invalido... ");
-//        }
+        try {
+            String conteudoCarterias = Arquivo.lerArquivo(CARTEIRAS_PATH);
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(Negociacao.class,
+                            new PropertyBasedInterfaceMarshal())
+                    .create();
+
+            Type listCarteiras = new TypeToken<ArrayList<Carteira>>() {
+            }.getType();
+
+            carteiras = gson.fromJson(conteudoCarterias, listCarteiras);
+
+        } catch (IOException ex) {
+            System.out.println("Arquivo invalido... ");
+        }
     }
 }
